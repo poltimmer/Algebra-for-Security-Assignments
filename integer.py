3242fdc1cb7c0752e35dd34232c3bea851aca42c
Pol Timmer <pol.timmer@gmail.com>
2019-09-12 12:23:08 +0200

    Migrated helper functions to new file

--- integer.py
+++ integer.py
@@ -1,5 +1,6 @@
 from math import ceil
 from math import floor
+from utils import *
 
 
 def parse_input():
@@ -215,69 +216,5 @@ def karatsuba(x, y, radix):
 #         while is_greater_than(r, y):
 #             r = subtract(r, y, radix)
 #     return True
-
-
-# Returns boolean whether x is greater than y, where x and y are numbers in list representation, with the digit of
-# lowest significance being first in the list.
-def is_greater_than(x, y):
-    for a, b in zip(reversed(x), reversed(y)):
-        if a > b:
-            return True
-        elif a < b:
-            return False
-    return False
-
-
-def is_equal(x, y):
-    for a, b in zip(x, y):
-        if a != b:
-            return False
-    return True
-
-
-def invert(x):
-    result = []
-    for number in x:
-        result.append(-number)
-    return result
-
-
-# converts a string number of base [radix] to an array of integers, still of base [radix]
-def number_to_array(n, radix):
-    result = []
-    for digit in n:
-        result.append(int(digit, radix))
-    result.reverse()
-    return result
-
-
-# does the opposite of number_to_array
-# doesn't need radix, as input array is an array of integers that can be larger than 9
-def array_to_number(a):
-    result = ''
-    a.reverse()
-    for digit in a:
-        result += dec_to_string(digit)
-    return result
-
-
-# takes a single digit, represented as an int (could be higher than 10) and returns it as a string character,
-# allowing up to base 16.
-def dec_to_string(n):
-    if n < 10:
-        return str(n)
-    elif n == 10:
-        return "a"
-    elif n == 11:
-        return "b"
-    elif n == 12:
-        return "c"
-    elif n == 13:
-        return "d"
-    elif n == 14:
-        return "e"
-    elif n == 15:
-        return "f"
-
 
 parse_input()
