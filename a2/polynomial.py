dc5609d12cf9eb4149c642d72a1d22f248ce7bae
Luke Houben <luke@lukehouben.nl>
2019-10-10 12:31:08 +0200

    codestyle

--- a2/polynomial.py
+++ a2/polynomial.py
@@ -423,14 +423,12 @@ def equals_field(a_remote, b_remote, m, poly_mod):
     return 'hoi'
 
 def add_field(mod_poly,m,a,b):
-
     a_and_b = add_poly(a,b,m)
     _, answer = long_div_poly(a_and_b, mod_poly, m)
 
     return answer
 
 def subtract_field(mod_poly,m,a,b):
-
     a_and_b = subtract_poly(a,b,m)
     _, answer = long_div_poly(a_and_b, mod_poly, m)
 
